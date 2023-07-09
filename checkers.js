13a5c2b1a0c7ad5a3ea53a9d66f9abcb48d51563
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-07-09 10:27:23 +0200

    /

--- checkers.js
+++ checkers.js
@@ -34,10 +34,36 @@ const _memoryUsage = () => {
 };
 
 
+const _healthcheck = async(body)=>{
+    const {uptime,cpuload,diskspace,memoryusage} = body;
+    const health = {};
+    if(uptime){
+        health['uptime'] = {value:_uptime(),metric:'seconds'};
+    }
+    if(cpuload){
+        health['cpuload'] = {value: await _cpuLoad(), metric: 'percentage'};
+    }
+    if(diskspace){
+        const diskSpace = await _diskSpace();
+        health['diskspace'] = {
+            total: {value: diskSpace.total, metric: 'GB'},
+            used: {value: diskSpace.used, metric: 'GB'},
+            free: {value: diskSpace.free, metric: 'GB'}
+        };
+    }
+    if(memoryusage){
+        const memoryUsage = _memoryUsage();
+        health['memoryusage'] = {
+            rss: {value: memoryUsage.rss, metric: 'MB'},
+            heapTotal: {value: memoryUsage.heapTotal, metric: 'MB'},
+            heapUsed: {value: memoryUsage.heapUsed, metric: 'MB'},
+            external: {value: memoryUsage.external, metric: 'MB'}
+        };
+    }
+    return health;
+}
+
 
 module.exports={
-    _uptime,
-    _cpuLoad,
-    _memoryUsage,
-    _diskSpace,
+    _healthcheck,
 }
--- health-checker.js
+++ health-checker.js
@@ -1,32 +1,8 @@
 const route = require('express').Router();
-const {_uptime,_cpuLoad,_diskSpace,_memoryUsage} = require('./checkers')
+const {_healthcheck} = require('./checkers')
 
 const healthcheck = async(req,res)=>{
-    const health = {};
-    const {uptime,cpuload,diskspace,memoryusage} = req.body;
-    if(uptime){
-        health['uptime'] = {value:_uptime(),metric:'seconds'};
-    }
-    if(cpuload){
-        health['cpuload'] = {value: await _cpuLoad(), metric: 'percentage'};
-    }
-    if(diskspace){
-        const diskSpace = await _diskSpace();
-        health['diskspace'] = {
-            total: {value: diskSpace.total, metric: 'GB'},
-            used: {value: diskSpace.used, metric: 'GB'},
-            free: {value: diskSpace.free, metric: 'GB'}
-        };
-    }
-    if(memoryusage){
-        const memoryUsage = _memoryUsage();
-        health['memoryusage'] = {
-            rss: {value: memoryUsage.rss, metric: 'MB'},
-            heapTotal: {value: memoryUsage.heapTotal, metric: 'MB'},
-            heapUsed: {value: memoryUsage.heapUsed, metric: 'MB'},
-            external: {value: memoryUsage.external, metric: 'MB'}
-        };
-    }
+    const health = await _healthcheck(req.body)
     return res.status(200).json(health)
 }
 
