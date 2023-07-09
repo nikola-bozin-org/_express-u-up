dc2cb897b5376e1e3c14e00c9d918cb78948f9c1
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-07-09 10:12:31 +0200

    /

--- checkers.js
+++ checkers.js
@@ -1,9 +1,31 @@
+const si = require('systeminformation');
+
 const _uptime =()=>{
     return process.uptime();
+}
+const _cpuLoad = async () => {
+    const load = await si.currentLoad();
+    return load.currentLoad;
+}
+const _diskSpace = async () => {
+    const drives = await si.fsSize();
+    const mainDrive = drives[0];
+    
+    const totalSpaceGB = mainDrive.size ? mainDrive.size / 1024 / 1024 / 1024 : 0;
+    const usedSpaceGB = mainDrive.size ? (mainDrive.size - mainDrive.available) / 1024 / 1024 / 1024 : 0;
+    const freeSpaceGB = mainDrive.size ? mainDrive.available / 1024 / 1024 / 1024 : 0;
+
+    return {
+        total: totalSpaceGB,
+        used: usedSpaceGB,
+        free: freeSpaceGB
+    };
 }
 
 
 
 module.exports={
     _uptime,
+    _cpuLoad,
+    _diskSpace,
 }
--- health-checker.js
+++ health-checker.js
@@ -1,11 +1,22 @@
 const route = require('express').Router();
-const {_uptime} = require('./checkers')
+const {_uptime,_cpuLoad,_diskSpace} = require('./checkers')
 
-const healthcheck = (req,res)=>{
+const healthcheck = async(req,res)=>{
     const health = {};
-    const {uptime} = req.query;
-    if(uptime){
+    const {uptime,cpuload,diskspace} = req.query;
+    if(uptime==='1'){
         health['uptime'] = {value:_uptime(),metric:'seconds'};
+    }
+    if(cpuload==='1'){
+        health['cpuload'] = {value: await _cpuLoad(), metric: 'percentage'};
+    }
+    if(diskspace){
+        const diskSpace = await _diskSpace();
+        health['diskspace'] = {
+            total: {value: diskSpace.total, metric: 'GB'},
+            used: {value: diskSpace.used, metric: 'GB'},
+            free: {value: diskSpace.free, metric: 'GB'}
+        };
     }
     return res.status(200).json(health)
 }
@@ -13,7 +24,6 @@ const healthcheck = (req,res)=>{
 const configure = (config)=>{
     const {path} = config;
     route.get(path,healthcheck)
-    console.info(path)
 }
 
 module.exports = {
